2ecd41e969fe99b5096339f217f679b28861c9e3
John Allison <john@jallison.co.uk>
2023-07-10 21:46:51 +0100

    Initial commit

---
+++ Application.cs
@@ -0,0 +1,11 @@
+﻿namespace BundleARMACommands;
+
+internal class Application
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Starting ARMA Command Bundler");
+        var x = await Scraper.GetRawData(CancellationToken.None);
+        x.ForEach(Console.WriteLine);
+    }
+}
---
+++ Scraper.cs
@@ -0,0 +1,34 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BundleARMACommands;
+
+public static class Scraper
+{
+    private static readonly string CommandsUrl = "https://community.bistudio.com/wiki/Category:Scripting_Commands";
+    private static readonly char[] Filter = { '►', ' ', '+', '-' };
+
+    public static async Task<List<string>> GetRawData(CancellationToken cancellationToken)
+    {
+        var client = new HttpClient();
+        var response = await client.GetStringAsync(CommandsUrl, cancellationToken);
+
+        var rawHtml = new HtmlDocument();
+        rawHtml.LoadHtml(response);
+
+        var nodes = rawHtml.DocumentNode.SelectNodes("//li[not(contains(@class, 'bohemia-header-nav-item'))]")
+		    .ToList();
+
+        var commands = nodes.Where(node => node.InnerHtml.Trim() != string.Empty
+                && !node.InnerText.Any(n => Filter.Contains(n))
+                && !char.IsUpper(node.InnerText.FirstOrDefault()))
+            .Select(node => node.InnerText.Trim())
+            .ToList();
+
+        return commands;
+    }
+}
