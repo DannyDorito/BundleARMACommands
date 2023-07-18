ccba5a3b453f205190a6bb0c8bcb1c2e240ef77d
John Allison <john@jallison.co.uk>
2023-07-18 18:32:52 +0100

    cleanup

--- Application.cs
+++ Application.cs
@@ -1,31 +1,23 @@
 ﻿using BundleARMACommands;
 
-internal class Program
+Console.WriteLine(value: "Starting ARMA Command Bundler");
+
+var commands = new List<string>();
+
+foreach (var uri in Scraper.UrisToScrape)
 {
-    private static async Task Main(string[] args)
-    {
-        Console.WriteLine("Starting ARMA Command Bundler");
-
-        var commands = new List<string>();
-
-        foreach (var uri in Scraper.UrisToScrape)
-        {
-            commands.AddRange( await Scraper.GetRawData(uri, CancellationToken.None).ConfigureAwait(true));
-        }
-
-        commands.Sort(StringComparer.OrdinalIgnoreCase);
-
-        var hackyCommands = new List<string>();
-
-        foreach (var c in commands)
-        {
-            hackyCommands.Add($"<KeyWord name=\"{c}\" />");
-        }
-
-        var hackyCommandsStr = string.Join("\r\n", hackyCommands);
-
-        File.WriteAllText(@$"{Environment.CurrentDirectory}\output.txt", hackyCommandsStr);
-
-        //await Writer.WriteToXML(commands, args[0]).ConfigureAwait(true);
-    }
+    commands.AddRange(await Scraper.GetRawData(uri, CancellationToken.None).ConfigureAwait(true));
 }
+
+commands.Sort(StringComparer.OrdinalIgnoreCase);
+
+var hackyCommands = new List<string>();
+
+foreach (var c in commands)
+{
+    hackyCommands.Add($"<KeyWord name=\"{c}\" />");
+}
+
+var hackyCommandsStr = string.Join("\r\n", hackyCommands);
+
+File.WriteAllText(@$"{Environment.CurrentDirectory}\output.txt", hackyCommandsStr);
--- Writer.cs
+++ Writer.cs
@@ -1,19 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata;
+﻿using System.Collections.ObjectModel;
 using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
-using System.Xml.Linq;
 
 namespace BundleARMACommands;
 
 public static class Writer
 {
 
-    public static async Task WriteToXML(List<string> commands, string file)
+    public static void WriteToXML(ReadOnlyCollection<string> commands, string file)
     {
+        if (commands is null)
+        {
+            throw new ArgumentNullException(nameof(commands));
+        }
+
+        if (string.IsNullOrEmpty(file))
+        {
+            throw new ArgumentException($"'{nameof(file)}' cannot be null or empty.", nameof(file));
+        }
+
         EncodingProvider ppp = CodePagesEncodingProvider.Instance;
         Encoding.RegisterProvider(ppp);
 
@@ -28,7 +33,8 @@ public static class Writer
             myData.Load(reader);
 
             var x = myData.ChildNodes[1]!.ChildNodes[0]!;
-            foreach ( XmlNode x2 in x.ChildNodes) {
+            foreach (XmlNode x2 in x.ChildNodes)
+            {
                 if (x2.Name != "Environment")
                 {
                     existingKeyWords.Add(x2.Attributes?.GetNamedItem("name")?.Value!);
