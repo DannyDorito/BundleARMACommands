f8b85716efd2919a69aa709b0d58bc3c64436d04
John Allison <john@jallison.co.uk>
2025-01-24 20:52:29 +0000

    Add no commit cli

--- BundleARMACommands/Application.cs
+++ BundleARMACommands/Application.cs
@@ -10,7 +10,7 @@ internal class Program
     {
         Console.WriteLine($"Starting {typeof(Program).Assembly.GetName().Name}.");
 
-        if (args.Length != 2)
+        if (args.Length < 2)
             throw new ArgumentNullException(nameof(args));
 
         var commands = new List<string>();
@@ -26,7 +26,23 @@ internal class Program
 
         var changed = Writer.WriteXML(commands, args.ElementAt(0), FileType.AutoComplete);
 
-        if (changed)
+        bool? commit = null;
+        try
+        {
+            commit = args.ElementAtOrDefault(2) != "no-commit";
+            if (!commit.Value)
+#pragma warning disable CA1303 // Do not pass literals as localized parameters
+                Console.WriteLine($"Found no commit at {nameof(args)}, skipping pushing changes to repo.");
+#pragma warning restore CA1303 // Do not pass literals as localized parameters
+        }
+        catch (ArgumentNullException)
+        {
+#pragma warning disable CA1303 // Do not pass literals as localized parameters
+            Console.WriteLine($"Did not find no commit at {nameof(args)}, pushing changes to repo.");
+#pragma warning restore CA1303 // Do not pass literals as localized parameters
+        }
+
+        if (changed && (commit.HasValue && commit.Value))
         {
             await Commit.PushToRepo(args.ElementAt(1), CancellationToken.None).ConfigureAwait(true);
             Console.WriteLine($"Pushed Changes to {args.ElementAt(1)} repo.");
