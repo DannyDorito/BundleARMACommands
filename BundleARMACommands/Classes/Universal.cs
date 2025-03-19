9102b1b89babd4fc7dbd0ca65f29a42ad539b0e2
John Allison <john@jallison.co.uk>
2025-03-19 21:22:10 +0000

    Update Universal.cs

--- BundleARMACommands/Classes/Universal.cs
+++ BundleARMACommands/Classes/Universal.cs
@@ -1,27 +1,26 @@
 ﻿using BundleARMACommands.Enums;
 using System.Collections.ObjectModel;
 
-namespace BundleARMACommands.Classes
+namespace BundleARMACommands.Classes;
+
+public static class Universal
 {
-    public static class Universal
-    {
-        public static readonly Collection<Website> WebsitesToScrape =
-        [
-            new Website(new(Resources.ScriptingCommandsUrl), WebsiteType.Commands),
-                new Website(new(Resources.FunctionsUrl), WebsiteType.Functions),
-                new Website(new(Resources.CBAUrl), WebsiteType.CBA)
-        ];
+    public static readonly Collection<Website> WebsitesToScrape =
+    [
+        new Website(new(Resources.ScriptingCommandsUrl), WebsiteType.Commands),
+            new Website(new(Resources.FunctionsUrl), WebsiteType.Functions),
+            new Website(new(Resources.CBAUrl), WebsiteType.CBA)
+    ];
 
-        public static ReadOnlyCollection<string> Filter => new(InternalFilter);
+    public static ReadOnlyCollection<string> Filter => new(InternalFilter);
 
-        private static List<string> InternalFilter => ["a != b", "! a", "a != b", "a % b", "a && b", "a &amp;&amp; b", "a * b", "a / b", "a : b", "a = b", "a == b", "a greater b", "a greater= b", "a hash b", "a less b", "a less= b", "a or b", "a ^ b", "+", "-", "User:OverlordZorn/Sandbox"];
+    private static List<string> InternalFilter => ["a != b", "! a", "a != b", "a % b", "a && b", "a &amp;&amp; b", "a * b", "a / b", "a : b", "a = b", "a == b", "a greater b", "a greater= b", "a hash b", "a less b", "a less= b", "a or b", "a ^ b", "+", "-", "User:OverlordZorn/Sandbox"];
 
-        public static string[] Prepend() => Resources.Prepend.Split(',');
+    public static string[] Prepend() => Resources.Prepend.Split(',');
 
-        public static string CBAAppend => Resources.CBAAppend;
+    public static string CBAAppend => Resources.CBAAppend;
 
-        public const string KeywordPrepend = "\t\t<KeyWord name=\"";
+    public const string KeywordPrepend = "\t\t<KeyWord name=\"";
 
-        public const string KeywordAppend = "\" />";
-    }
+    public const string KeywordAppend = "\" />";
 }
